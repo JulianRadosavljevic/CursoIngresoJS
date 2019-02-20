acf249abe16dfb843bdc8b6534bf96c0ab080c26
alumno <alumno>
2019-02-20 20:03:03 -0300

    Proyecto4

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,34 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+    {
+        case 'Febrero': 
+        {
+            alert("Este mes no tiene más de 30 días");
+            break;
+        }
+
+        case 'Enero': 
+        case 'Marzo':
+        case 'Abril':
+        case 'Mayo':
+        case 'Junio':
+        case 'Julio':
+        case 'Agosto':
+        case 'Septiembre':
+        case 'Octubre':
+        case 'Noviembre':
+        case 'Diciembre':
+        {
+            alert("Este mes tiene más de 30 días");
+            break;
+        }
+    }
 
 
 }//FIN DE LA FUNCIÓN
